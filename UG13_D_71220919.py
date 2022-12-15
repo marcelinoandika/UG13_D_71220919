9055a22b4d4bd84602790d340ff9f2d27b47b7fa
marcelinoandika <117901348+marcelinoandika@users.noreply.github.com>
2022-12-15 16:22:55 +0700

    Update UG13_D_71220919.py

---
+++ UG13_D_71220919.py
@@ -0,0 +1,70 @@
+import turtle
+
+k = turtle.Turtle()
+k.color("yellow")
+k.pencolor("orange")
+k.begin_fill()
+k.pen()
+
+k.penup()
+k.goto(-100,90)
+k.pendown()
+k.pensize(30)
+k.left(90)
+k.forward(150)
+k.right(135)
+k.forward(150)
+k.left(90)
+k.forward(150)
+k.right(135)
+k.forward(150)
+
+k.penup()
+k.goto(-210,0)
+k.pendown()
+k.right
+k.circle(50,360)
+
+k.circle(50,180)
+k.left(180)
+k.forward(60)
+for i in range(120):
+    k.forward(1)
+    k.right(1)
+
+
+
+k.penup()
+k.goto(180,90)
+k.pendown()
+k.left(90)
+k.forward(150)
+
+
+k.penup()
+k.goto(90,90)
+k.pendown()
+k.right
+k.circle(50,360)
+
+k.circle(50,180)
+k.left(180)
+k.forward(60)
+for i in range(120):
+    k.forward(1)
+    k.right(1)
+
+
+
+
+
+
+
+
+
+turtle.write("Marcell ganteng bgt")
+
+
+
+
+sc = turtle.Screen().exitonclick()
